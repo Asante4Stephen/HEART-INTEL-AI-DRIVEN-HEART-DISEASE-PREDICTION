1026d88de5d311a11f868812725bc11f3f7f6607
Asante Kwabena Stephen <kwabenastephenasante2019@gmail.com>
2024-10-18 09:26:51 +0000

    Add files via upload

---
+++ deploy/heart_app.py
@@ -0,0 +1,81 @@
+import uvicorn
+from fastapi import FastAPI
+from Heart_Params import HeartParameter
+import numpy as np
+import pickle
+import warnings
+warnings.filterwarnings('ignore')
+
+app = FastAPI()
+pickle_in = open("svc.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+#print(classifier)
+
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+
+@app.post('/predict')
+def predict_disease(data:HeartParameter):
+    age = data.age       
+    sex = data.sex
+    cp  = data.cp
+    trestbps = data.trestbps  
+    col  = data.chol 
+    fbs = data.fbs
+    restecg = data.restecg
+    thalach = data.thalach  
+    exang   = data.exang
+    oldpeak  = data.oldpeak
+    slope  =  data.slope 
+    ca   = data.ca  
+    thal  = data.thal
+
+
+
+    features = np.array([[age,sex,cp,trestbps,col,fbs,restecg,thalach,exang,oldpeak,
+    slope,ca,thal]])
+
+    prediction = classifier.predict(features)
+
+    prediction = prediction[0].item()
+
+    result = ''
+
+    details = {
+        "age":age,     
+        "sex": sex, 
+        "chest pain type":cp, 
+        "resting blood pressure": trestbps,
+        "serum colesterol": col,
+        "fasting blood sugar level" : fbs,
+        "resting electrocardiographoc results" : restecg,
+        "maximum heart rate": thalach,
+        "exercise induced agina" : exang,
+        "st depression" : oldpeak,
+        "slope" : slope,
+        "number of major vessels" : ca,
+        "thallium stress test_results" : thal,
+    }
+
+
+
+
+
+    if prediction == 1:
+        result = f'Unfortunately, you have heart disease'
+    else:
+        result = f'Your heart is fine, you do not have heart disease'
+
+    return {
+        'status': result,
+        'details': details
+    }
+
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload
